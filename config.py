dd1f5fbad27c46270f946e40168660f163f8b5fc
lightopen <light_open@outlook.com>
2016-06-08 00:14:19 +0800

    more file

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+import pymysql
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# parent class
+class Config(object):
+    SECRET_KEY = 'demo'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    @staticmethod
+    def init_app(app):
+        pass
+
+# design model
+class DesignConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'task.sqlite')
+
+config = {'default': DesignConfig}
